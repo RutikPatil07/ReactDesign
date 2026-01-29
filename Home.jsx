c50f9ee7e66e44b5de6cf9a1c02a9cda1ed9e3be
Mr. Rutik Patil <patilrutik500@gmail.com>
2026-01-29 15:37:39 +0530

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,16 @@
+import "./Home.css"
+export default function Home() {
+  return (
+    <>
+    <div class="d">
+      <h1 id ="h">Home Page </h1>
+      <p id = "h">Programming Langauge</p>
+      <ol>
+        <li>C</li><li>CPP</li>
+        <li>JAVA</li><li>PYTHON</li><li>SQL</li>
+      </ol>
+      </div>
+    </>
+  )
+}
+
