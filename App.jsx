a5eb9e7b828c8187924de0acfa8457c33d9c51c0
Mr. Rutik Patil <patilrutik500@gmail.com>
2026-01-29 15:40:13 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Home from "./Home"
+import "./App.css"
+import logo from "./assets/download.jpg"
+export default function App() {
+ 
+
+  return (
+    <>
+    <div class="d">
+      <h1 id="h">The React Page </h1>
+      <img src={logo} height={150} width={200}></img>
+      </div>
+    </>
+  )
+}
